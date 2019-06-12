c511b7be69276947a0a88eeec09ab7e43d86b7c6
elihe999 <eli.he999@hotmail.com>
2019-06-12 17:07:12 +0800

    <chore>Update API document

---
+++ doc_retrieval/api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "ATE API Document",
+  "version": "1.0.0",
+  "description": "ATE Backend",
+  "title": "API Document",
+  "url": "",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2019-06-12T08:48:41.044Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.7"
+  }
+});
